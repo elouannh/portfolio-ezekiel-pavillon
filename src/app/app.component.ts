6ed47cbde92410384115ee32ddb92c9b879c9fe3
Elouann <80158828+elouannh@users.noreply.github.com>
2024-06-01 13:03:12 +0200

    assets and styles setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RoomComponent } from "./components/room/room.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RoomComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio-ezekiel-pavillon';
