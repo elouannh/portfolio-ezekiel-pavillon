16fdb4167b1faf4827c7866e133fcf42a7ec2e2b
Elouann <80158828+elouannh@users.noreply.github.com>
2024-06-07 20:09:48 +0200

    pull the images and the image component

---
+++ src/app/components/custom-image/custom-image.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-image',
+  standalone: true,
+  imports: [],
+  templateUrl: './custom-image.component.html',
+  styleUrl: './custom-image.component.scss'
+})
+export class CustomImageComponent {
+
+}
