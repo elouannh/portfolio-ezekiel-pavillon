5d1ac7f64bdf3e1f4ea4457d6f052e26fc1e42e6
Elouann <80158828+elouannh@users.noreply.github.com>
2024-06-18 02:55:23 +0200

    Not supported platform.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RoomDotsComponent } from "./components/room-dots/room-dots.component";
 import {TopicPageComponent} from "./pages/topic-page/topic-page.component";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RoomDotsComponent, TopicPageComponent],
+  imports: [RouterOutlet, RoomDotsComponent, TopicPageComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  public innerWidth: any;
 
+  public ngOnInit(): void {
+      this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event): void {
+    this.innerWidth = window.innerWidth;
+  }
 }
